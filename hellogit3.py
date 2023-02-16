a400a409ba4a36b0bfe6f58a99a3b30d548da1eb
luiscr4k <crucesaltahonal@gmail.com>
2023-02-16 14:08:16 -0400

    A;adiendo cosas a extra

--- hellogit3.py
+++ hellogit3.py
@@ -1,3 +1,3 @@
 print("La locura la locura absoluta")
-print("A;adiendo el login v2 v3 v4")
+print("A;adiendo el login v2 v3 v5 v6 v7 v4")
 print("Revisando si este ficher oexiste en la rama comentarios")
