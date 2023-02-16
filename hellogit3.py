576f74fcdb734d191c09dc60ada281d17c057a37
luiscr4k <crucesaltahonal@gmail.com>
2023-02-16 14:08:44 -0400

    A;adiendo cosas acomentarios en la misma linea q extra

--- hellogit3.py
+++ hellogit3.py
@@ -1,3 +1,3 @@
 print("La locura la locura absoluta")
-print("A;adiendo el login v2 v3 v4")
+print("A;adiendo el login v2 v3 v4 v87 8787 45")
 print("Revisando si este ficher oexiste en la rama comentarios")
